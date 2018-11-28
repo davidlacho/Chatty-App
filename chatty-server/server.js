28dd361807c6480301ac9cebdb51583609a80440
David Lacho <david.lacho@gmail.com>
2018-11-27 18:16:11 -0800

    implemented username change

---
+++ chatty-server/server.js
@@ -0,0 +1,43 @@
+// server.js
+const express = require('express');
+const WebSockets = require('ws');
+const uuidv4 = require('uuid/v4');
+
+// Set the port to 3001
+const PORT = 3001;
+
+// Create a new express server
+const server = express()
+   // Make the express server serve static assets (html, javascript, css) from the /public folder
+  .use(express.static('public'))
+  .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
+
+// Create the WebSockets server
+const wss = new WebSockets.Server({ server });
+
+
+// Set up a callback that will run when a client connects to the server
+// When a client connects they are assigned a socket, represented by
+// the ws parameter in the callback.
+
+
+
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+
+  ws.on('message', function incoming(data) {
+    const parsedData = JSON.parse(data);
+    parsedData['id'] = uuidv4();
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSockets.OPEN) {
+        client.send(JSON.stringify(parsedData));
+        console.log('sent: ', JSON.stringify(parsedData));
+      }
+});
+
+
+  });
+  // Set up a callback for when a client closes the socket. This usually means they closed their browser.
+  ws.on('close', () => console.log('Client disconnected'));
+});
